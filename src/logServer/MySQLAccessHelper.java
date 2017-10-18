c0daf23d51dda17855c2ebb6f95971cc97669078
maciekkobierecki <maciekkobierecki@gmail.com>
2017-10-19 00:12:46 +0200

    0.0.6

--- src/logServer/MySQLAccessHelper.java
+++ src/logServer/MySQLAccessHelper.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.util.Map;
 
 public class MySQLAccessHelper {
 	private Connection connect=null;
@@ -13,15 +14,33 @@ public class MySQLAccessHelper {
 	private PreparedStatement preparedStatement=null;
 	private ResultSet resultSet=null;
 	
-	public void readDataBase() throws Exception {
+	public MySQLAccessHelper(){
+		loadDrivers();
+	}
+	
+	public void loadDrivers(){
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			
+		}
+		catch(Exception e){
+			System.out.println("database drivers not found");
+		}
+	}
+	public void readDataBase() throws Exception {
+		try{
 			connect=DriverManager.getConnection("jdbc:mysql//localhost/people?"+ "user=sqluser&password=sqluserpw");
 			
 			statement=connect.createStatement();
 			
 			resultSet=statement.executeQuery("select * from feedback.comments");
 		}
+		catch (Exception e){
+			throw e;
+		}
+	}
+	
+	public void insert(String tableName, Map<String,String>values) throws Exception {
+		connect=DriverManager.getConnection("jdbc:mysql//localhost/people?"+ "user=sqluser&password=sqluserpw");
+		String query= "insert into "+tableName+" values (default, >
 	}
 }
